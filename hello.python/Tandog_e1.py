82a01502d291120ca73ed5b4d05a0aaed81aa31d
rechelletandog <rechelleroblestandog9@gmail.com>
2018-10-25 22:43:34 +0800

    Exercise No.1

---
+++ hello.python/Tandog_e1.py
@@ -0,0 +1,10 @@
+
+
+print ("I am {}."
+.format("Rechelle Tandog"))
+print ("My spirit animal is {},because ."
+.format("Cat","they are so sweet"))
+print ("When not in school, I love to {}."
+.format("watch horror films"))
+print ("I dream being a {} in my future ."
+.format("Nurse"))
