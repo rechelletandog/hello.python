bef586ce233c46e54e5619b86968f764f287274e
rechelletandog <44455521+rechelletandog@users.noreply.github.com>
2018-10-26 10:30:20 +0800

    Add files via upload

---
+++ hello.python/Tandog_e2.py
@@ -0,0 +1,22 @@
+import sys
+
+Storm = sys.argv[1]
+Storm = int sys.argv[1]
+ 
+  if Storm == 61:
+         print ("Tropical Depression")  
+
+if Storm == 61:
+      print ( "Tropical Depression")
+
+elif Storm == 62 or Storm >= 88:
+      print ("Category ", "Tropical Storm")
+
+elif Storm == 89 or Storm >= 117:
+      print ("Category ", "Tropical Storm")
+
+elif Storm == 118 or Storm >= 220: 
+      print ("Category ", "Tropical Storm")
+
+elif Storm == 220:
+      print ("Category ", "Tropical Storm")
